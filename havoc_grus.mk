88010716a8de29028fb9464be8b9ea2c4fac9505
weitaming2002 <71814801+weitaming2002@users.noreply.github.com>
2021-03-27 17:20:18 +0800

    Update havoc_grus.mk

--- havoc_grus.mk
+++ havoc_grus.mk
@@ -38,8 +38,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Havoc
-export export HAVOC_BUILD_TYPE=Official
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.havoc.maintainer=kyoto44
